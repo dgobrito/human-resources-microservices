49c868e7cd4dab17acfced86131d56943660a33c
Dyego dos Santos Brito <dyego.eti@gmail.com>
2023-08-21 21:23:11 -0300

    Eureka Server created

--- user-api/src/main/java/br/com/dgobrito/userapi/services/impl/UserServiceImpl.java
+++ user-api/src/main/java/br/com/dgobrito/userapi/services/impl/UserServiceImpl.java
@@ -3,6 +3,7 @@ package br.com.dgobrito.userapi.services.impl;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
 import br.com.dgobrito.userapi.domain.User;
@@ -10,16 +11,22 @@ import br.com.dgobrito.userapi.repositories.UserRepository;
 import br.com.dgobrito.userapi.services.UserService;
 import br.com.dgobrito.userapi.services.exceptions.ObjectNotFoundException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class UserServiceImpl implements UserService {
+	
+	@Autowired
+	private final Environment env;
 	
 	@Autowired
 	private final UserRepository repository;
 
 	@Override
 	public User findById(Long id) {
+		log.info("USER_SERVICE ::: Get request on " + env.getProperty("local.server.port") + " port");
 		return repository.findById(id).orElseThrow(() -> new ObjectNotFoundException("Object not found"));
 	}
 
